931f29d98c03509b31d045275ef57f3d181eabaf
Aline Chahine <aline.chahine@proton.me>
2025-10-25 14:31:31 -0300

    Atualizações

--- Exercicios/Algoritmo_41.py
+++ Exercicios/Algoritmo_41.py
@@ -9,7 +9,7 @@ numero02 = int(input("Digite o segundo número: "))
 numero03 = int(input("Digite o terceiro número: "))
 numero04 = int(input("Digite o quarto número: "))
 
-media = ((numero01*1) + (numero02*2) + (numero03*3) + (numero04*4)) / 4
+mediaP = ((numero01*1) + (numero02*2) + (numero03*3) + (numero04*4)) / 4
 
 print(f"Média ponderada: {media}")
 
--- Exercicios/Algoritmo_50.py
+++ Exercicios/Algoritmo_50.py
@@ -4,9 +4,15 @@ perímetro
 area
 diagonal'''
 
-
 import os 
 os.system('cls')
+import math
 
-base = float(input("Digite a base do retângulo: "))
-altura = float(input("Digite a altura do retângulo: "))
+x = float(input("Digite o valor da base do retângulo: "))
+y = float(input("Digite o valor da altura do retângulo: "))
+
+perimetro = 2 * (x + y)
+area = x * y
+diagonal = math.sqrt(x*x + y*y)
+
+print(f"Perímetro: {perimetro}\nÁrea: {area}\nDiagonal: {diagonal}")
