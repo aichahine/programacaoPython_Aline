ef990c6d893001b01edf1c95a6ec75e4544b6d09
Aline Chahine <aline.chahine@proton.me>
2025-10-26 15:04:51 -0300

    Mais algoritmos da lista de exercícios

--- Exercicios/Algoritmo_59.py
+++ Exercicios/Algoritmo_59.py
@@ -4,3 +4,11 @@
 import os 
 os.system('cls')
 import math
+
+# Recebendo os valores
+a = int(input("Digite o valor do primeiro cateto: "))
+b = int(input("Digite o valor do segundo cateto: "))
+
+# Calculando a hipotenusa
+hipotenusa = math.sqrt(((a**2) + (b**2)))
+print(f"A hipotenusa é: {hipotenusa}")
---
+++ Exercicios/Funcoes_Math.py
@@ -0,0 +1,11 @@
+# COLINHA DE FUNÇÕES MATH
+# seno                      sin
+# coseno                    cos
+# tangente                  tan
+# logaritmo na base 10      log
+# logaritmo em outras bases log(número,base)
+# raiz                      sqrt
+# pi                        pi
+# truncar                   trunc
+# arrendondar para cima     ceil
+# arredondar para baixo     floor
